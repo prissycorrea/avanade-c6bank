570c8f4f209790dd95c2d800ef55213511b4f1cc
Priscilla Correa Delfino <p.correa.delfino@avanade.com>
2022-10-14 16:29:44 -0300

    deploy

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+    background-image: url('/barco.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
 
 export default function RegisterPage() {
     return (
-        <div>register</div>
+        <Container>
+            register
+        </Container>
     )
 }
